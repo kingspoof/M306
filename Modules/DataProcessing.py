adefe7fe393baef9c344ce41cb185aa85ec26c16
Lucien Käser <lucien.kaeser@me.com>
2021-06-08 14:32:59 +0200

    initial

---
+++ Modules/DataProcessing.py
@@ -0,0 +1,15 @@
+#fill the following fields from the dataobject
+# 
+# spezialwords = [[]] With only the spezial words and their position
+# words = [[]] With other words until only the other words
+
+# words and spezial words have to be arrays of char arrays and they have to be
+# the same size
+
+class DataProcessing:
+    def __init__(self):
+        print("init dataprocessing")
+
+
+    def process(dataobject):
+        return dataobject
